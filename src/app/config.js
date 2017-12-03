d99ac1894cc79f08ec3448d6ae633a6e2293a09a
Gyula Varadi <gyula.varadi@lhsystems.com>
2017-12-03 02:06:09 +0100

    build(deps): adding a promise-polyfill and jquery as dependecies

--- src/app/config.js
+++ src/app/config.js
@@ -1,6 +1,13 @@
 requirejs.config({
     baseUrl: './',
     paths: {
-        app: 'src/app'
+        app: 'src/app',
+        promise: 'libs/promise.min',
+        jquery: 'libs/jquery-3.2.1.min',
+        shim: {
+            promise: {
+                exports: 'Promise',
+            }
+        }
     }
 });

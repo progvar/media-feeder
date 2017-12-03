bf7f2e1b253c6ef4ff80305e5fa292951906e1a1
Gyula Varadi <gyula.varadi@lhsystems.com>
2017-12-03 18:03:20 +0100

    build(deps): remove the promise polyfill since we don't need it yet..

--- src/app/config.js
+++ src/app/config.js
@@ -2,12 +2,6 @@ requirejs.config({
     baseUrl: './',
     paths: {
         app: 'src/app',
-        promise: 'libs/promise.min',
-        jquery: 'libs/jquery-3.2.1.min',
-        shim: {
-            promise: {
-                exports: 'Promise',
-            }
-        }
+        jquery: 'libs/jquery-3.2.1.min'
     }
 });

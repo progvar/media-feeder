8c90c825dc0319c1965d1066f571735869590c07
Gyula Varadi <gyula.varadi@lhsystems.com>
2017-12-03 02:07:43 +0100

    feat(api): handle our first call to the api

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,17 @@
-define(function() {
-    console.log('TODO.. :/');
-});
+define(['jquery'], fetchFeed);
+
+
+function fetchFeed($) {
+    let url = 'http://146.185.158.18/fake_api.php';
+
+    $.ajax({
+        url,
+        dataType: 'jsonp',
+        success: handleFeed
+    });
+}
+
+
+function handleFeed(feed) {
+    console.log(feed);
+}

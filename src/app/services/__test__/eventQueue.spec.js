0bbc7547a625814289e7f66223bad016c58710e8
Gyula Varadi <gyula.varadi@lhsystems.com>
2017-12-21 17:31:27 +0100

    test(eventQueueSvc): add more subscribe() tests

--- src/app/services/__test__/eventQueue.spec.js
+++ src/app/services/__test__/eventQueue.spec.js
@@ -4,17 +4,47 @@ require("amd-loader");
 
 
 let { expect } = require('chai');
-
-let eventQueueSvc = rewire('../eventQueue.svc');
+let eventQueueSvc = require('../eventQueue.svc');
 
 
 describe('eventQueueSvc', () => {
+    let subscription;
+
     after(() => {
         eventQueueSvc.eventQueue = {};
     })
-    describe('subscribe', () => {
-        let subscription;
 
+    describe('calling subscribe with wrong params', () => {
+
+        before(() => {
+            let topic = '',
+                listenerFn = () => {};
+
+            subscription = eventQueueSvc.subscribe(topic, listenerFn);
+        })
+
+        it('should return if the `topic` is not a string or fasly', () => {
+            let topic = 'test_topic',
+                listenerFn = 'not a function';
+
+            subscription = eventQueueSvc.subscribe(topic, listenerFn);
+
+            expect(eventQueueSvc.eventQueue).to.be.an('object');
+            expect(eventQueueSvc.eventQueue).to.eql({});
+        })
+
+        it('should return if the `listenerFn` is not a function or falsy', () => {
+            let topic = '',
+                listenerFn = () => {};
+
+            subscription = eventQueueSvc.subscribe(topic, listenerFn);
+
+            expect(eventQueueSvc.eventQueue).to.be.an('object');
+            expect(eventQueueSvc.eventQueue).to.eql({});
+        })
+    })
+
+    describe('calling subscribe with corrent params', () => {
         before(() => {
             let topic = 'test_topic',
                 listenerFn = () => {};
@@ -38,6 +68,15 @@ describe('eventQueueSvc', () => {
         it('should return a method called `unsubscribe`', () => {
             expect(subscription).to.have.property('unsubscribe');
             expect(subscription.unsubscribe).to.be.a('function');
+        })
+
+        it('should delelete the listener when calling `unsubscribe`', () => {
+            subscription.unsubscribe()
+
+            let testTopic = eventQueueSvc.eventQueue['test_topic'];
+
+            expect(testTopic).to.be.an('array');
+            expect(testTopic.length).to.eql(0);
         })
     })
 })
--- src/app/services/eventQueue.svc.js
+++ src/app/services/eventQueue.svc.js
@@ -8,12 +8,16 @@ function eventQueueService() {
     let eventQueue = {};
 
     function subscribe(topic, listenerFn) {
+        if (!isType(topic, 'string') || !isType(listenerFn, 'function')) {
+            return;
+        }
+
         eventQueue[topic] = eventQueue[topic] || [];
 
-        let listenerIndex = eventQueue[topic].push(listenerFn);
+        let listenerIndex = eventQueue[topic].push(listenerFn) - 1;
 
         function unsubscribe() {
-            delete eventQueue[topic][listenerIndex];
+            delete eventQueue[topic].splice(listenerIndex, 1);
         }
 
         return {
@@ -29,10 +33,15 @@ function eventQueueService() {
         eventQueue[topic].forEach(listenerFn => listenerFn(data));
     }
 
+    function isType(target, type) {
+        return target && type && typeof target === type;
+    }
+
     return {
         eventQueue,
         subscribe,
-        publish
+        publish,
+        isType
     }
 
 }
